d5ae1b5a6eaffe138376a001a17a62e7d3e0e6ce
Juan <jnbetancourt08@gmail.com>
2024-02-19 15:59:44 -0500

    patron_factory

--- factory/CreacionPago.java
+++ factory/CreacionPago.java
@@ -1,18 +1,21 @@
 package factory;
 
 import java.nio.file.NoSuchFileException;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class CreacionPago {
-    
+
+    private final static Map<MetodoPago, Pago> pagos =  new HashMap<>(){{
+            put(MetodoPago.PAYPAL, new Paypal());
+            put(MetodoPago.TARJETA_CREDITO, new TarjetaCredito());
+            put(MetodoPago.BITCOIN, new Bitcoin());
+            put(MetodoPago.APPLE_PAY, new ApplePay());}};
+
     public static Pago crearPago(MetodoPago metodoPago) throws NoSuchFileException{
 
-        return switch (metodoPago) {
-            case PAYPAL -> new Paypal();
-            case TARJETA_CREDITO -> new TarjetaCredito();
-            case BITCOIN -> new Bitcoin();
-            case APPLE_PAY -> new ApplePay();
-            default -> throw new NoSuchFileException("Tipo de pago no encontrado");
-        };
+        return pagos.get(metodoPago);
 
     }
 
